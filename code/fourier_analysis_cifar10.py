ffbb20de82a0fc5abffc5449a6ae275533cb5978
Jiachen Sun <jiachens@oahu.eecs.umich.edu>
2021-06-23 12:38:21 -0400

    add plot file for certified accuracy heatmap

--- code/fourier_analysis_cifar10.py
+++ code/fourier_analysis_cifar10.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-06-15 18:55:35
 LastEditors: Jiachen Sun
-LastEditTime: 2021-06-18 22:54:10
+LastEditTime: 2021-06-23 11:45:42
 '''
 import numpy as np
 import os
@@ -17,7 +17,6 @@ from architectures import get_architecture
 from torchvision.utils import save_image
 import torch
 from skimage.color import rgb2gray
-import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -44,6 +43,7 @@ if __name__ == "__main__":
     dataset_orig = get_dataset("cifar10", "test")
     
     sum_ps2D = 0
+    sum_ps2D_orig = 0
     for i in range(len(dataset)):
         (x, label) = dataset[i]
         # save_image(
@@ -61,24 +61,33 @@ if __name__ == "__main__":
         
         # print(x - x_orig)
         img_grey = rgb2gray(np.round((x - x_orig) * 255)) / 255
+        img_grey_orig = rgb2gray(np.round((x_orig) * 255)) / 255
 
         img_grey_F = np.fft.fftshift(np.fft.fft2(img_grey))
+        img_grey_F_orig = np.fft.fftshift(np.fft.fft2(img_grey_orig))
         ps2D = np.abs(img_grey_F)
+        ps2D_orig = np.abs(img_grey_F_orig)
         sum_ps2D += ps2D
+        sum_ps2D_orig += ps2D_orig
         
     avg_ps2D = sum_ps2D / len(dataset)
-    print('Max value: {}'.format(np.max(avg_ps2D)))
-    print('Min value: {}'.format(np.min(avg_ps2D)))
-    avg_ps2D = np.clip(avg_ps2D,0,2)
-    print('Max value: {}'.format(np.max(avg_ps2D)))
-    print('Min value: {}'.format(np.min(avg_ps2D)))
+    avg_ps2D_orig = sum_ps2D_orig / len(dataset)
 
-    ax = sns.heatmap(avg_ps2D,
+    relative = np.divide(avg_ps2D,avg_ps2D_orig)
+    # print(avg_ps2D.shape)
+    # avg_ps2D[16,16] = 0.
+    # print('Max value: {}'.format(np.max(avg_ps2D)))
+    # print('Min value: {}'.format(np.min(avg_ps2D)))
+    # avg_ps2D = np.clip(avg_ps2D,0,2)
+
+    ax = sns.heatmap(relative,
                 cmap="jet",
                 cbar=True,
-                cbar_kws={"ticks":[]},
+                vmin = 0., 
+                vmax = 50.,
+                # cbar_kws={"ticks":[]},
                 xticklabels=False,
                 yticklabels=False,)
-    plt.savefig('./test/fourier_analysis/' + args.dataset + '_' + args.corruption +  '_' + str(args.severity) + '_clip_1.png',dpi=250,bbox_inches='tight')
+    plt.savefig('./test/fourier_analysis/' + args.dataset + '_' + args.corruption +  '_' + str(args.severity) + '_relative_unified.png',dpi=250,bbox_inches='tight')
     # plt.savefig('./figures/fourier_analysis/' + args.corruption +  '_' + args.severity + '.png',dpi=250,bbox_inches='tight')    
     plt.close()
