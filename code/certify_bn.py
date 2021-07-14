5f534e73f4a93e29775c14a9900da77f28d2333d
Jiachen Sun <jiachens@oahu.eecs.umich.edu>
2021-07-14 18:03:05 -0400

    add tent and bn

--- code/certify_bn.py
+++ code/certify_bn.py
@@ -1,24 +1,25 @@
 '''
 Description: 
 Autor: Jiachen Sun
-Date: 2021-07-13 22:48:22
+Date: 2021-07-14 17:53:15
 LastEditors: Jiachen Sun
-LastEditTime: 2021-07-14 17:55:02
+LastEditTime: 2021-07-14 17:54:46
 '''
-# evaluate a smoothed classifier on a dataset
 import argparse
 import os
 import setGPU
+from torch.nn import parameter
 from datasets import get_dataset, DATASETS, get_num_classes
 from core import Smooth
 from time import time
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import datetime
+from third_party import bn_helper
 from architectures import get_architecture,ARCHITECTURES
-from third_party import ttt_helper
+import torch.optim as optim
 import numpy as np
+
 
 
 parser = argparse.ArgumentParser(description='Certify many examples')
@@ -31,7 +32,7 @@ parser.add_argument("--path", type=str, help="path to dataset")
 parser.add_argument("--corruption", type=str, default="fog", help="corruption type when using cifar10-c")
 parser.add_argument("--severity", type=int, default=1, help="severity level when using cifar10-c")
 parser.add_argument("--batch", type=int, default=1000, help="batch size")
-parser.add_argument("--batch_size", type=int, default=500, help="batch size for TTT")
+parser.add_argument("--batch_size", type=int, default=500, help="batch size for Tent")
 parser.add_argument("--skip", type=int, default=1, help="how many examples to skip")
 parser.add_argument("--max", type=int, default=-1, help="stop after this many examples")
 parser.add_argument("--split", choices=["train", "test"], default="test", help="train or test set")
@@ -41,62 +42,53 @@ parser.add_argument("--niter", type=int, default=10, help="number of steps for a
 parser.add_argument("--alpha", type=float, default=0.001, help="failure probability")
 parser.add_argument("--gpu", type=str, default='0', help="which GPU to use")
 parser.add_argument("--no_normalize", default=True, action='store_false')
-parser.add_argument('--shared', default='layer2', type=str,
-                    help='branching point')
+
 args = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
 
-
-def adapt(data,arch,dir,net,ext,ssh):
-    criterion_ssh = nn.CrossEntropyLoss().cuda()
-    if True:
-        ssh.eval()
-        ext.train()
-        optimizer_ssh = optim.SGD(ext.parameters(), lr=0.001)
-    # else:
-    #     ssh.train()
-    #     optimizer_ssh = optim.SGD(ssh.parameters(), lr=0.001)
-
-    for iteration in range(args.niter):
-        index = np.random.choice(len(data),args.batch_size,replace=False)
-        inputs = [data[index[j]][0].permute(2,0,1) for j in range(args.batch_size)]
-        inputs = torch.stack(inputs)
-        inputs_ssh, labels_ssh = ttt_helper.rotate_batch(inputs, 'rand')
-        inputs_ssh, labels_ssh = inputs_ssh.cuda(), labels_ssh.cuda()
-        optimizer_ssh.zero_grad()
-        outputs_ssh = ssh(inputs_ssh)
-        loss_ssh = criterion_ssh(outputs_ssh, labels_ssh)
-        loss_ssh.backward()
-        optimizer_ssh.step()
+def adapt(data,dir,model):
+    model = bn_helper.configure_model(model)
+    index = np.random.choice(len(data),args.batch_size,replace=False)
+    inputs = [data[index[j]][0].permute(2,0,1) for j in range(args.batch_size)]
+    inputs = torch.stack(inputs)
+    model(inputs)
     print("Adaptation Done ...")
     torch.save({
         'arch': args.arch,
-        'state_dict': net.state_dict(), 
-        'optimizer': optimizer_ssh.state_dict(),
-    }, os.path.join(dir, 'ttt_checkpoint_{}_{}.pth.tar'.format(args.corruption, args.severity)))
-    return net
-
-# def test(model, image, label):
-# 	model.eval()
-# 	inputs = ttt_helper.te_transforms(image).unsqueeze(0)
-# 	with torch.no_grad():
-# 		outputs = model(inputs.cuda())
-# 		_, predicted = outputs.max(1)
-# 		confidence = nn.functional.softmax(outputs, dim=1).squeeze()[label].item()
-# 	correctness = 1 if predicted.item() == label else 0
-# 	return correctness, confidence
-
+        'state_dict': model.state_dict(), 
+    }, os.path.join(dir, 'bn_checkpoint_{}_{}.pth.tar'.format(args.corruption, args.severity)))
+    return model
 
 if __name__ == "__main__":
     # load the base classifier
-    net, ext, head, ssh = ttt_helper.build_model(args)
     checkpoint = torch.load(args.base_classifier)
-    net.load_state_dict(checkpoint['state_dict'])
-    head.load_state_dict(checkpoint['head'])
-    dir = args.outfile[:-(len(args.outfile.split('/')[-1])+1)]
-    if not os.path.exists(dir):
-        os.makedirs(dir,exist_ok = True)
+    try:
+        base_classifier = get_architecture(checkpoint["arch"], args.dataset, args.no_normalize)
+        base_classifier.load_state_dict(checkpoint['state_dict'])
+    except:
+        base_classifier = get_architecture("cifar_resnet110", args.dataset, args.no_normalize)
+        # print(checkpoint['model_state_dict'].keys())
+        from collections import OrderedDict
+        new_state_dict = OrderedDict()
+        if 'model_state_dict' in checkpoint.keys():
+            for key, val in checkpoint['model_state_dict'].items():
+                # print(key)
+                if key[:6] == 'module':
+                    name = key[7:]  # remove 'module.'
+                else:
+                    name = key
+                new_state_dict[name] = val
+        else:
+            for key, val in checkpoint['state_dict'].items():
+                # print(key)
+                if key[:6] == 'module':
+                    name = key[7:]  # remove 'module.'
+                else:
+                    name = key
+                new_state_dict[name] = val
+        base_classifier.load_state_dict(new_state_dict)
+
     # iterate through the dataset
     if args.dataset == "cifar10-c":
         # print(args.path)
@@ -105,17 +97,18 @@ if __name__ == "__main__":
         dataset = get_dataset(args.dataset, None, args.path, args.corruption, args.severity)
     else:
         dataset = get_dataset(args.dataset, args.split)
-
-    net = adapt(dataset,checkpoint['arch'],dir,net,ext,ssh)
-    net.eval()
-    head.eval()
+        
+    dir = args.outfile[:-(len(args.outfile.split('/')[-1])+1)]
+    if not os.path.exists(dir):
+        os.makedirs(dir,exist_ok = True)
+        
+    base_classifier = adapt(dataset,dir,base_classifier)
     # create the smooothed classifier g
-    smoothed_classifier = Smooth(net, get_num_classes(args.dataset), args.sigma)
+    smoothed_classifier = Smooth(base_classifier, get_num_classes(args.dataset), args.sigma)
 
     # prepare output file
     f = open(args.outfile, 'w')
     print("idx\tlabel\tpredict\tradius\tcorrect\ttime", file=f, flush=True)
-    
     
     total = 0
     total_correct = 0
@@ -161,5 +154,4 @@ if __name__ == "__main__":
     print("Correct Radius: {}".format(total_r/total_correct), file=f, flush=True)
     print("Correct Radius (with 0 for incorrect): {}".format(total_r/total), file=f, flush=True)
     print("Radius: {}".format(total_r_with_incorrect/total), file=f, flush=True)
-
     f.close()
