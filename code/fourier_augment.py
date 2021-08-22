b2e326ffdb8ff5a236aed8522f465a57774ff0d8
Jiachen Sun <jcsun0806@gmail.com>
2021-08-22 16:48:06 -0400

    fourier

--- code/fourier_augment.py
+++ code/fourier_augment.py
@@ -3,13 +3,15 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-07-30 16:37:09
 LastEditors: Jiachen Sun
-LastEditTime: 2021-08-19 23:15:04
+LastEditTime: 2021-08-22 15:40:48
 '''
 import torch
 import fourier_basis
 import numpy as np
 import random
 import torchvision
+from torch.distributions.dirichlet import Dirichlet
+from torch.distributions.beta import Beta
 
 pre = torchvision.transforms.Compose([
                 torchvision.transforms.RandomCrop(32, padding=4),
@@ -39,7 +41,23 @@ class FourierDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.dataset)
 
-def augment(x_orig, k, p, basis):
+
+def augment(x_orig, k, p, basis,chain = 3):
+
+    x_aug = torch.zeros_like(x_orig)
+    mixing_weight_dist = Dirichlet(torch.empty(chain).fill_(1.))
+    mixing_weights = mixing_weight_dist.sample()
+    for i in range(chain):
+        x_temp = augment_single(x_orig)
+        x_aug += mixing_weights[i] * x_temp
+
+    skip_conn_weight_dist = Beta(torch.tensor([1.]), torch.tensor([1.]))
+    skip_conn_weight = skip_conn_weight_dist.sample()
+
+    x_fourier = skip_conn_weight * x_aug + (1 - skip_conn_weight) * x_orig
+    return x_fourier
+
+def augment_single(x_orig):
 
     ######### Fourier #########
     # p = np.random.choice(p)
@@ -49,13 +67,13 @@ def augment(x_orig, k, p, basis):
     severity_3 = random.choice(range(1,6))
     c = [0.2,0.3,0.4,0.5,0.6][severity_1-1]
     d = [6,5,4,3,2][severity_2-1]
-    e = [22,20,18,16,14][severity_3-1]
+    #e = [22,20,18,16,14][severity_3-1]
     x_orig_1 = x_orig.clone().numpy()
     x_orig_f = np.fft.fftshift(np.fft.fft2(x_orig_1))
     x_orig_f_abs = np.abs(x_orig_f) 
     x_orig_f_ang = np.angle(x_orig_f) 
-    flag = np.random.uniform() - 0.5
-    x_orig_f_abs *= 1. + np.sign(flag) * np.random.rand(*x_orig_f_abs.shape) * c
+    flag = np.sign(np.random.uniform() - 0.5)
+    x_orig_f_abs *= 1. + flag * np.random.rand(*x_orig_f_abs.shape) * c
     x_orig_f_ang += (np.random.rand(*x_orig_f_ang.shape) - 0.5) * np.pi / d
     x_orig_f.real = x_orig_f_abs * np.cos(x_orig_f_ang)
     x_orig_f.imag = x_orig_f_abs * np.sin(x_orig_f_ang)
@@ -80,10 +98,10 @@ def augment(x_orig, k, p, basis):
     ##############################
 
     # b = np.random.uniform()
-    b = 0
-    x_restored = x_restored_1 * b + x_restored_2 * (1 - b)
+    # # b = 0
+    # x_restored = x_restored_1 * b + x_restored_2 * (1 - b)
 
-    a = np.random.uniform()
-    x_restored = x_restored * a + x_orig * (1-a)
+    # # a = np.random.uniform()
+    # # x_restored = x_restored * a + x_orig * (1-a)
 
-    return x_restored
+    return x_restored_2
