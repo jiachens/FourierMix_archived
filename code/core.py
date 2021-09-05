675c1cf500ccaa4c2226862326a57d1911a363cc
Jiachen Sun <jcsun0806@gmail.com>
2021-09-05 18:08:51 -0400

    jsd in c

--- code/core.py
+++ code/core.py
@@ -102,6 +102,7 @@ class Smooth(object):
                 batch = x.repeat((this_batch_size, 1, 1, 1))
                 noise = torch.randn_like(batch, device='cuda') * self.sigma
                 predictions = self.base_classifier(batch + noise).argmax(1)
+                print(predictions.shape)
                 counts += self._count_arr(predictions.cpu().numpy(), self.num_classes)
             return counts
 
