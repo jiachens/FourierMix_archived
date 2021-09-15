6bbad60859b7a6cb9e6ea53a873f8d24ca7be261
Jiachen Sun <jcsun0806@gmail.com>
2021-09-15 17:22:35 -0400

    experimental code

--- code/fourier_augment.py
+++ code/fourier_augment.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-07-30 16:37:09
 LastEditors: Jiachen Sun
-LastEditTime: 2021-09-15 13:00:54
+LastEditTime: 2021-09-15 17:22:25
 '''
 import torch
 import fourier_basis
@@ -86,9 +86,9 @@ def augment_single(x_orig):
     x_orig_f_ang += (np.random.rand(*x_orig_f_ang.shape) - 0.5) * np.pi / d
     x_orig_f.real = x_orig_f_abs * np.cos(x_orig_f_ang)
     x_orig_f.imag = x_orig_f_abs * np.sin(x_orig_f_ang)
-    row = np.random.choice(32,e,replace=True)
-    col = np.random.choice(32,e,replace=True)
-    x_orig_f[:,row,col] = 0
+    # row = np.random.choice(32,e,replace=True)
+    # col = np.random.choice(32,e,replace=True)
+    # x_orig_f[:,row,col] = 0
     x_restored_1 = np.abs(np.fft.ifft2(np.fft.ifftshift(x_orig_f)))
     x_restored_1 = torch.FloatTensor(x_restored_1) 
 
