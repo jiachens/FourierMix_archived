d808db8dc45d81d49b9dcce83f824af2d00c2ede
Jiachen Sun <jcsun0806@gmail.com>
2021-10-12 00:04:27 -0400

    certify fouriermix imagenet

--- analysis/plot_power_law.py
+++ analysis/plot_power_law.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-10-07 22:19:25
 LastEditors: Jiachen Sun
-LastEditTime: 2021-10-10 23:30:15
+LastEditTime: 2021-10-11 22:43:01
 '''
 import os
 import re
@@ -25,7 +25,7 @@ y2 = power_law(x,1)
 y3 = power_law(x,2)
 y4 = power_law(x,3)
 
-plt.figure(figsize=(12,3))
+plt.figure(figsize=(12,6))
 
 ax = plt.gca()#获取边框
 # ax.spines['top'].set_color('red')  
@@ -42,18 +42,18 @@ ax.yaxis.set_major_locator(MultipleLocator(15))
 
 plt.grid( color = 'black',linestyle='-.',linewidth = 0.5)
 
-l0=plt.plot(x,y0, label='$alpha$=0.1',markersize=9,lw=2)
-l1=plt.plot(x,y1, label='$alpha$=0.5',markersize=9,lw=2)
-l2=plt.plot(x,y2, label='$alpha$=1',markersize=9,lw=2)
-l3=plt.plot(x,y3, label='$alpha$=2',markersize=9,lw=2)
-l4=plt.plot(x,y4, label='$alpha$=3',markersize=9,lw=2)
+l0=plt.plot(x,y0, label=r'$\alpha$=0.1',markersize=9,lw=3)
+l1=plt.plot(x,y1, label=r'$\alpha$=0.5',markersize=9,lw=3)
+l2=plt.plot(x,y2, label=r'$\alpha$=1',markersize=9,lw=3)
+l3=plt.plot(x,y3, label=r'$\alpha$=2',markersize=9,lw=3)
+l4=plt.plot(x,y4, label=r'$\alpha$=3',markersize=9,lw=3)
 
 
-plt.xlabel('x')
-plt.ylabel('y') 
+plt.xlabel('center frequency f',fontsize=25)
+plt.ylabel('power law noise',fontsize=25) 
 # plt.xlabel('Adversarial Budget $\epsilon$ (PGD-200)',fontsize=27.5)
 # plt.ylabel('Adversarial Accuracy (%)',fontsize=27.5)
-plt.legend()
+plt.legend(fontsize=25)
 
 # plt.xlim(0, 2001)
 plt.ylim(0, 1)
@@ -62,6 +62,6 @@ plt.ylim(0, 1)
 # plt.yticks(my_y_ticks)
 # plt.xticks(x,my_x_ticks)
 # plt.legend(fontsize=27.5, bbox_to_anchor=(1.1,1.0),borderaxespad = 0.)
-plt.xticks(range(0, 16, 2),['$f_c$','$f_c ± 2$','$f_c ± 4$','$f_c ± 6$','$f_c ± 8$','$f_c ± 10$','$f_c ± 12$','$f_c ± 14$'])#, color="red", rotation=45)
-plt.yticks(range(0, 2))#, color="red", rotation=45)
+plt.xticks(range(0, 16, 2),['$f_c$','$f_c ± 2$','$f_c ± 4$','$f_c ± 6$','$f_c ± 8$','$f_c ± 10$','$f_c ± 12$','$f_c ± 14$'],fontsize=25)#, color="red", rotation=45)
+plt.yticks([0,0.5,1],fontsize=25)#, color="red", rotation=45)
 plt.savefig('./test_power_law.png',dpi=300,bbox_inches='tight')
