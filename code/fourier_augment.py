06ba29a40c0a21d23666e607d3e754551aa69938
Jiachen Sun <jcsun0806@gmail.com>
2021-10-17 11:01:29 -0400

    add new cifar10f

--- code/fourier_augment.py
+++ code/fourier_augment.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-07-30 16:37:09
 LastEditors: Jiachen Sun
-LastEditTime: 2021-10-12 14:32:51
+LastEditTime: 2021-10-17 11:01:18
 '''
 import torch
 import fourier_basis
@@ -80,9 +80,9 @@ def augment_single(x_orig):
     x_orig_f_abs = np.abs(x_orig_f) 
     x_orig_f_ang = np.angle(x_orig_f) 
     flag = np.sign(np.random.uniform() - 0.5)
-    x_orig_f_abs *= 1. + flag * np.random.rand(*x_orig_f_abs.shape) * c * 3
+    x_orig_f_abs *= 1. + flag * np.random.rand(*x_orig_f_abs.shape) * c 
     # x_orig_f_abs += (np.random.uniform(*x_orig_f_abs.shape) - 0.5) * f * MASK 
-    x_orig_f_ang += (np.random.rand(*x_orig_f_ang.shape) - 0.5) * np.pi / d * 3
+    x_orig_f_ang += (np.random.normal(*x_orig_f_ang.shape) - 0.5) * np.pi / d 
     x_orig_f.real = x_orig_f_abs * np.cos(x_orig_f_ang)
     x_orig_f.imag = x_orig_f_abs * np.sin(x_orig_f_ang)
     # row = np.random.choice(32,e,replace=True)
