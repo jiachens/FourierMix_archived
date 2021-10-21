a21dd1bff341a2ed5571904665ea161be355fb6c
Jiachen Sun <jcsun0806@gmail.com>
2021-10-21 03:11:47 -0400

    add new cifar10f

--- code/fourier_augment_cuda.py
+++ code/fourier_augment_cuda.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-10-12 17:37:13
 LastEditors: Jiachen Sun
-LastEditTime: 2021-10-21 03:10:27
+LastEditTime: 2021-10-21 03:11:43
 '''
 import torch
 import fourier_basis
@@ -85,14 +85,12 @@ def augment_single(x_orig,device=None):
     # print(x_orig_f_abs)
     x_orig_f_ang = torch.angle(x_orig_f) 
     flag = np.sign(np.random.uniform() - 0.5)
-    # x_orig_f_abs *= 1. + flag * torch.rand(*x_orig_f_abs.shape).to(device) * c
-    x_orig_f_abs = 100.
-    # x_orig_f_ang += (torch.rand(*x_orig_f_ang.shape).to(device) - 0.5) * np.pi 
-    x_orig_f_ang = torch.zeros_like(x_orig_f_ang)
+    x_orig_f_abs *= 1. + flag * torch.rand(*x_orig_f_abs.shape).to(device) * c
+    x_orig_f_ang += (torch.rand(*x_orig_f_ang.shape).to(device) - 0.5) * np.pi 
     # print(x_orig_f_ang)
     x_orig_f.real = x_orig_f_abs * torch.cos(x_orig_f_ang)
     x_orig_f.imag = x_orig_f_abs * torch.sin(x_orig_f_ang)
-    print(x_orig_f)
+    # print(x_orig_f)
     x_restored_1 = torch.abs(torch.fft.ifftn(x_orig_f, s=None, dim=(2,3), norm=None))
     # print(x_restored_1 - x_orig_1)
     # print(x_orig_f-torch.fft.fftn(x_orig_1, s=None, dim=(-2,-1), norm=None))
