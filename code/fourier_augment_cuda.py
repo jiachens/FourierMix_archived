1784c671e068264be58053b561862654cd31d25c
Jiachen Sun <jcsun0806@gmail.com>
2021-10-21 02:51:20 -0400

    add new cifar10f

--- code/fourier_augment_cuda.py
+++ code/fourier_augment_cuda.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-10-12 17:37:13
 LastEditors: Jiachen Sun
-LastEditTime: 2021-10-21 02:50:13
+LastEditTime: 2021-10-21 02:51:15
 '''
 import torch
 import fourier_basis
@@ -88,7 +88,7 @@ def augment_single(x_orig,device=None):
     x_orig_f_abs *= 1. + flag * torch.rand(*x_orig_f_abs.shape).to(device) * c
     # x_orig_f_abs = 1.
     # x_orig_f_ang += (torch.rand(*x_orig_f_ang.shape).to(device) - 0.5) * np.pi 
-    x_orig_f_ang =  np.pi 
+    x_orig_f_ang =  torch.Tensor(np.pi) 
     x_orig_f.real = x_orig_f_abs * torch.cos(x_orig_f_ang)
     x_orig_f.imag = x_orig_f_abs * torch.sin(x_orig_f_ang)
     x_restored_1 = torch.abs(torch.fft.ifftn(x_orig_f, s=None, dim=(-2,-1), norm=None))
