e92187d1b6a313840247331541fc8483296b71b7
Jiachen Sun <jcsun0806@gmail.com>
2021-11-16 16:28:06 -0500

    add imagenet c bar

--- analysis/plot_cifarf.py
+++ analysis/plot_cifarf.py
@@ -3,7 +3,7 @@ Description:
 Autor: Jiachen Sun
 Date: 2021-10-12 14:38:44
 LastEditors: Jiachen Sun
-LastEditTime: 2021-11-16 14:27:58
+LastEditTime: 2021-11-16 16:28:01
 '''
 import os
 import re
@@ -46,13 +46,13 @@ for alpha in [0.5, 1, 2, 3]:
 
     plt.grid( color = 'black',linestyle='-.',linewidth = 0.25)
 
-    l1=plt.plot(x,y[0], marker='^', label='Gaussain',markersize=12,lw=3, color='#845EC2')
-    l2=plt.plot(x,y[1], marker='^',label='+JSD',markersize=12,lw=3,color='#4B4453')
-    l3=plt.plot(x,y[2],marker='^', label='+AutoAugment+JSD',markersize=12,lw=3,color='#B0A8B9')
-    l4=plt.plot(x,y[3],marker='^', label='+AugMix+JSD',markersize=12,lw=3,color='#C34A36')
-    l41=plt.plot(x,y[4],marker='^', label='+AugMix+HCR',markersize=12,lw=3,color='#4AA233')
-    l5=plt.plot(x,y[5], marker='^',label='+FourierMix+JSD',markersize=12,lw=3,color='#FF8066')
-    l6=plt.plot(x,y[6],marker='^', label='+FourierMix+HCR',markersize=12,lw=3,color='#4E8397')
+    l1=plt.plot(x,y[0], marker='^', label='Gaussain',markersize=14,lw=4, color='#845EC2')
+    l2=plt.plot(x,y[1], marker='^',label='+JSD',markersize=14,lw=4,color='#4B4453')
+    l3=plt.plot(x,y[2],marker='^', label='+AutoAugment+JSD',markersize=12,lw=4,color='#B0A8B9')
+    l4=plt.plot(x,y[3],marker='^', label='+AugMix+JSD',markersize=14,lw=4,color='#C34A36')
+    l41=plt.plot(x,y[4],marker='^', label='+AugMix+HCR',markersize=14,lw=4,color='#4AA233')
+    l5=plt.plot(x,y[5], marker='^',label='+FourierMix+JSD',markersize=14,lw=4,color='#FF8066')
+    l6=plt.plot(x,y[6],marker='^', label='+FourierMix+HCR',markersize=14,lw=4,color='#4E8397')
 
     for i in range(len(y)):
         print(name[i],np.mean(y[i]))
